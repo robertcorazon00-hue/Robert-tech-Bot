711bdf933faa65d3636650d995f9e8e11161a5c1
robertcorazon00-hue <robertcorazon00@gmail.com>
2026-02-07 07:49:15 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+import connectToWhatsapp from './Robert tech/crew.js'
+import handleIncomingMessage from './events/messageHandler.js'
+
+(async() => {
+    await connectToWhatsapp(handleIncomingMessage)
+        console.log('established !')
+})()
